ab92db306caf364c985e43c48e76cc48c2268bf9
Matt <matthewrothstein@hotmail.com>
2015-10-17 21:16:48 -0400

    linked pictures to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true,greater_than: 0}
-  has_many :reviews, :images
+  has_many :reviews
+  has_many :images
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
     format("%.2f", price_in_dollars)
