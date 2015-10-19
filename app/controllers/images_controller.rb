3445014366696aa01008ea14262327000d1b8b66
Matt <matthewrothstein@hotmail.com>
2015-10-19 19:43:02 -0400

    changes to file structure

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -43,7 +43,7 @@ class ImagesController < ApplicationController
   def update
     respond_to do |format|
       if @image.update(image_params)
-        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.html { redirect_to @image }
         format.json { render :show, status: :ok, location: @image }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class ImagesController < ApplicationController
   def destroy
     @image.destroy
     respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.html { redirect_to images_url}
       format.json { head :no_content }
     end
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,10 @@ class Product < ActiveRecord::Base
   validates :price_in_cents, numericality: {only_integer: true,greater_than: 0}
   has_many :reviews
   has_many :images
+
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
     format("%.2f", price_in_dollars)
   end
-
 
 end
