99ad84591801828726110630e8f320871addf7ea
Matt <matthewrothstein@hotmail.com>
2015-10-19 17:30:04 -0400

    database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151018003629) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20151018003629) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "images", ["product_id"], name: "index_images_on_product_id"
+  add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -38,7 +41,7 @@ ActiveRecord::Schema.define(version: 20151018003629) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id"
+  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -55,7 +58,9 @@ ActiveRecord::Schema.define(version: 20151018003629) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "images", "products"
+  add_foreign_key "reviews", "products"
 end
