860d27ba3b31cdbfb69de68150e28b802b4b17cc
Matt <matthewrothstein@hotmail.com>
2015-10-20 19:24:03 -0400

    new cart feature for app

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :products do
     resources :images
   end
+
+  get 'buy/:id' => 'products#buy', as: 'purchase'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
